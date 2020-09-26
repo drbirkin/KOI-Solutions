cc79485442e795032450a969d3a0aa5eb0918de5
Dr_Birkin <davidliat98@gmail.com>
2020-09-26 17:38:51 +0200

    edit css mobile

--- koisolutionsjs/koisolutions.js
+++ koisolutionsjs/koisolutions.js
@@ -1,38 +1,50 @@
-$(".q1").click(function () {
-  $(".cost").slideToggle("slow");
-});
+$(function () {
 
-$(".q2").click(function () {
-  $(".restock").slideToggle("slow");
-});
+  AOS.init();
 
-$(".q3").click(function () {
-  $(".support").slideToggle("slow");
-});
+  $(".q1").click(function () {
+    $(".cost").slideToggle("slow");
+  });
+
+  $(".q2").click(function () {
+    $(".restock").slideToggle("slow");
+  });
+
+  $(".q3").click(function () {
+    $(".support").slideToggle("slow");
+  });
 
 
-$(document).ready(function () {
-  $("a").on('click', function (event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-        window.location.hash = hash;
+  $(document).ready(function () {
+    $("a").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
+  var menu = "close";
+  $(".mobile-navbar .menu-toggle").click(function () {
+
+    if (menu === "close") {
+      $(this).parent().next(".mobile-nav").css({
+        "display": "flex",
+        "transform": "translate(0, 0)"
       });
+      menu = "open";
+    } else {
+      $(this).parent().next(".mobile-nav").css({
+        "display": "none",
+        "transform": "translate(0, -999%)"
+      });
+      menu = "close";
     }
   });
-});
 
-var menu = "close";
-$(".mobile-navbar .menu-toggle").click(function () {
-
-  if (menu === "close") {
-    $(this).parent().next(".mobile-nav").css("transform", "translate(0, 0)");
-    menu = "open";
-  } else {
-    $(this).parent().next(".mobile-nav").css("transform", "translate(0, -999%)");
-    menu = "close";
-  }
 });
